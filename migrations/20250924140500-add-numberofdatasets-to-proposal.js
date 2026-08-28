2dc70e1821d476f653b439594454b47da5a78f57
Johannes Reppin <johannesreppin@gmail.com>
2026-08-28 10:17:46 +0200

    update proposals in batches

--- migrations/20250924140500-add-numberofdatasets-to-proposal.js
+++ migrations/20250924140500-add-numberofdatasets-to-proposal.js
@@ -1,16 +1,31 @@
 module.exports = {
   async up(db, client) {
-
-    for await (const proposal of db.collection("Proposal").find({})) {
+    const BATCH_SIZE = 100;
+    
+    // Get total count
+    const totalProposals = await db.collection("Proposal").countDocuments({});
+    
+    // Process in batches
+    for (let i = 0; i < totalProposals; i += BATCH_SIZE) {
+      const proposals = await db
+        .collection("Proposal")
+        .find({})
+        .skip(i)
+        .limit(BATCH_SIZE)
+        .toArray();
       
-      const datasetCount = await db
-        .collection("Dataset")
-        .countDocuments({ proposalIds: proposal.proposalId });
-
-      await db.collection("Proposal").updateOne(
-        { _id: proposal._id },
-        { $set: { numberOfDatasets: datasetCount } }
-      );
+      for (const proposal of proposals) {
+        const datasetCount = await db
+          .collection("Dataset")
+          .countDocuments({ proposalIds: proposal.proposalId });
+        
+        await db.collection("Proposal").updateOne(
+          { _id: proposal._id },
+          { $set: { numberOfDatasets: datasetCount } }
+        );
+      }
+      
+      console.log(`Processed ${Math.min(i + BATCH_SIZE, totalProposals)}/${totalProposals} proposals`);
     }
   },
 
